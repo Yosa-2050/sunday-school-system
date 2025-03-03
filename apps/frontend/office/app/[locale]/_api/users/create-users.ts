fec8c047f2faa11396b4f07893762f2a98aff9fc
Ermias <wteffera11@gmail.com>
2025-03-03 18:09:52 +0300

    fix(shega): biom format fix

--- apps/frontend/office/app/[locale]/_api/users/create-users.ts
+++ apps/frontend/office/app/[locale]/_api/users/create-users.ts
@@ -10,11 +10,16 @@ export type CreateUsers = {
 };
 
 export const createUsers = async (data: CreateUsers) => {
-    const response: CreateUsers[] = await fetcher(data.role === 'WORK_PROVIDER' ? '/organization/createEmployee' : '/profile/new', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    });
+    const response: CreateUsers[] = await fetcher(
+        data.role === 'WORK_PROVIDER'
+            ? '/organization/createEmployee'
+            : '/profile/new',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        },
+    );
 
     return response;
 };
