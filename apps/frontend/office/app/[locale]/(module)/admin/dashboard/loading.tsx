035ce09e69250926667651138dc03ac52a7e83fc
Ermias <wteffera11@gmail.com>
2025-03-03 10:11:31 +0300

    fix(shega): auth layout update

--- apps/frontend/office/app/[locale]/(module)/admin/dashboard/loading.tsx
+++ apps/frontend/office/app/[locale]/(module)/admin/dashboard/loading.tsx
@@ -1,9 +1,7 @@
-import { LoadingOverlay } from "@mantine/core"
+import { LoadingOverlay } from '@mantine/core';
 
 const Loading = () => {
-  return (
-    <LoadingOverlay visible={true} h={"100%"}/>
-  )
-}
+    return <LoadingOverlay visible={true} h={'100%'} />;
+};
 
-export default Loading
+export default Loading;
