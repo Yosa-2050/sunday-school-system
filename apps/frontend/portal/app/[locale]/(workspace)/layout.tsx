4f2b5ac157bf2b237e312bdb1c6f8d15f0a1068b
Ermias <wteffera11@gmail.com>
2025-03-15 13:34:31 +0300

    fix(shega): fix portal layout

---
+++ apps/frontend/portal/app/[locale]/(workspace)/layout.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mantine/core";
+import type React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <Box>{children}</Box>;
+};
+
+export default Layout;
