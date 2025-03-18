acafe9bfa7229dd8bb58dc86aabb054526ec4229
Ermias <wteffera11@gmail.com>
2025-03-18 11:09:05 +0300

    fix(shega): biome format fix

--- packages/ui/src/components/EntityFilter.tsx
+++ packages/ui/src/components/EntityFilter.tsx
@@ -1,98 +1,99 @@
-"use client";
+'use client';
 
-import { Select } from "@mantine/core";
-import { entityParamSchema } from "@shega/shared";
-import { IconFilter } from "@tabler/icons-react";
-import { parseAsJson, useQueryState } from "nuqs";
-import { cn } from "../utilities/cn";
-import { MultiSelectPills } from "./MultiSelect";
+import { Select } from '@mantine/core';
+import { entityParamSchema } from '@shega/shared';
+import { IconFilter } from '@tabler/icons-react';
+import { parseAsJson, useQueryState } from 'nuqs';
+import { cn } from '../utilities/cn';
+import { MultiSelectPills } from './MultiSelect';
 
 type EntityFilterProps = {
-  filterOptions: { label: string; value: string }[];
-  mode: "select" | "multi";
-  entity: string;
-  field: string;
-  className?: string;
-  item?: string;
-  placeholder?: string;
+    filterOptions: { label: string; value: string }[];
+    mode: 'select' | 'multi';
+    entity: string;
+    field: string;
+    className?: string;
+    item?: string;
+    placeholder?: string;
 };
 
 export function EntityFilter({
-  mode = "select",
-  filterOptions,
-  className,
-  entity,
-  field,
-  item,
-  placeholder,
-}: EntityFilterProps) {
-  const [entityParams, setEntityParams] = useQueryState(
+    mode = 'select',
+    filterOptions,
+    className,
     entity,
-    parseAsJson(entityParamSchema.parse)
-  );
-
-  //   const defaultOption = filterOptions[0];
-
-  //   if (!defaultOption) {
-  //     throw new Error("filterOptions must not be empty");
-  //   }
-
-  // For multi-select, ensure the value is always an array
-  const filterParam =
-    entityParams?.f?.filter((f) => f.f === field)?.map((f) => f.v) || [];
-
-  const filterOperator = entityParams?.f?.[0]?.o || "eq";
-
-  const handleFilterChange = (value: string | string[] | null) => {
-    const existingFilters = entityParams?.f?.filter((f) => f.f !== field) || [];
-
-    if (!value || (Array.isArray(value) && value.length === 0)) {
-      const updatedParams = { ...entityParams };
-      updatedParams.f =
-        existingFilters.length > 0 ? existingFilters : undefined;
-
-      setEntityParams({ ...updatedParams, p: 1 });
-    } else {
-      const values = Array.isArray(value) ? value : [value];
-
-      const newFilters = values.map((v) => ({
-        f: field,
-        v: v,
-        o: filterOperator,
-      }));
-
-      setEntityParams({
-        ...entityParams,
-        f: [...existingFilters, ...newFilters],
-      });
-    }
-  };
-
-  if (mode === "multi") {
-    return (
-      <MultiSelectPills
-        placeholder={placeholder || "Filter By"}
-        item={item}
-        value={filterParam} // Pass the array of values
-        data={filterOptions}
-        onChange={handleFilterChange}
-        // className={cn(className)}
-        // leftSection={<IconFilter size={18} stroke="1.25" />}
-      />
+    field,
+    item,
+    placeholder,
+}: EntityFilterProps) {
+    const [entityParams, setEntityParams] = useQueryState(
+        entity,
+        parseAsJson(entityParamSchema.parse),
     );
-  }
 
-  return (
-    <>
-      <Select
-        size="sm"
-        placeholder={placeholder || "Filter By"}
-        value={filterParam[0]} // Pass the first value for single select
-        data={filterOptions}
-        onChange={handleFilterChange}
-        className={cn(className)}
-        leftSection={<IconFilter size={18} stroke="1.25" />}
-      />
-    </>
-  );
+    //   const defaultOption = filterOptions[0];
+
+    //   if (!defaultOption) {
+    //     throw new Error("filterOptions must not be empty");
+    //   }
+
+    // For multi-select, ensure the value is always an array
+    const filterParam =
+        entityParams?.f?.filter((f) => f.f === field)?.map((f) => f.v) || [];
+
+    const filterOperator = entityParams?.f?.[0]?.o || 'eq';
+
+    const handleFilterChange = (value: string | string[] | null) => {
+        const existingFilters =
+            entityParams?.f?.filter((f) => f.f !== field) || [];
+
+        if (!value || (Array.isArray(value) && value.length === 0)) {
+            const updatedParams = { ...entityParams };
+            updatedParams.f =
+                existingFilters.length > 0 ? existingFilters : undefined;
+
+            setEntityParams({ ...updatedParams, p: 1 });
+        } else {
+            const values = Array.isArray(value) ? value : [value];
+
+            const newFilters = values.map((v) => ({
+                f: field,
+                v: v,
+                o: filterOperator,
+            }));
+
+            setEntityParams({
+                ...entityParams,
+                f: [...existingFilters, ...newFilters],
+            });
+        }
+    };
+
+    if (mode === 'multi') {
+        return (
+            <MultiSelectPills
+                placeholder={placeholder || 'Filter By'}
+                item={item}
+                value={filterParam} // Pass the array of values
+                data={filterOptions}
+                onChange={handleFilterChange}
+                // className={cn(className)}
+                // leftSection={<IconFilter size={18} stroke="1.25" />}
+            />
+        );
+    }
+
+    return (
+        <>
+            <Select
+                size="sm"
+                placeholder={placeholder || 'Filter By'}
+                value={filterParam[0]} // Pass the first value for single select
+                data={filterOptions}
+                onChange={handleFilterChange}
+                className={cn(className)}
+                leftSection={<IconFilter size={18} stroke="1.25" />}
+            />
+        </>
+    );
 }
