8a1f77d4dbba3a2301e1aa2fee7db979efff1f88
Ermias <wteffera11@gmail.com>
2025-03-23 08:55:40 +0300

    build fix

--- apps/frontend/portal/app/[locale]/auth/_components/Login.tsx
+++ apps/frontend/portal/app/[locale]/auth/_components/Login.tsx
@@ -1,151 +1,153 @@
-"use client";
+'use client';
 
-import { useRouter } from "@/i18n/routing";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from '@/i18n/routing';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
-  Anchor,
-  Box,
-  Button,
-  Flex,
-  Group,
-  PasswordInput,
-  Stack,
-  Text,
-  TextInput,
-  Title,
-} from "@mantine/core";
-import { notifications } from "@mantine/notifications";
+    Anchor,
+    Box,
+    Button,
+    Flex,
+    Group,
+    PasswordInput,
+    Stack,
+    Text,
+    TextInput,
+    Title,
+} from '@mantine/core';
+import { notifications } from '@mantine/notifications';
 import {
-  COOKIE_ACCESS_TOKEN,
-  COOKIE_REFRESH_TOKEN,
-  logger,
-  type User,
-} from "@shega/shared";
-import { useAuth } from "@shega/ui";
-import { useMutation } from "@tanstack/react-query";
-import { login } from "app/_api/auth/login";
-import { getUserAction } from "app/_api/get-user-action";
-import { setCookie } from "cookies-next";
-import { useTranslations } from "next-intl";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+    COOKIE_ACCESS_TOKEN,
+    COOKIE_REFRESH_TOKEN,
+    type User,
+    logger,
+} from '@shega/shared';
+import { useAuth } from '@shega/ui';
+import { useMutation } from '@tanstack/react-query';
+import { login } from 'app/_api/auth/login';
+import { getUserAction } from 'app/_api/get-user-action';
+import { setCookie } from 'cookies-next';
+import { useTranslations } from 'next-intl';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 const schema = z.object({
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .email("Invalid email format")
-    .regex(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email format"
-    ),
-  password: z.string().min(1, "Password  is required"),
-  rememberMe: z.boolean().optional(),
+    email: z
+        .string()
+        .min(1, 'Email is required')
+        .email('Invalid email format')
+        .regex(
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            'Invalid email format',
+        ),
+    password: z.string().min(1, 'Password  is required'),
+    rememberMe: z.boolean().optional(),
 });
 
 const Login = () => {
-  const { setUser } = useAuth();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const t = useTranslations("auth.login");
-  const [rememberMe, setRememberMe] = useState(false);
-  const [redirectPath, setRedirectPath] = useState("/");
+    const { setUser } = useAuth();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const t = useTranslations('auth.login');
+    const [rememberMe, setRememberMe] = useState(false);
+    const [redirectPath, setRedirectPath] = useState('/');
 
-  useEffect(() => {
-    const returnTo = searchParams.get("returnTo");
-    if (returnTo) {
-      setRedirectPath(decodeURIComponent(returnTo));
-    }
-  }, [searchParams]);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: zodResolver(schema) });
-
-  const loginMutation = useMutation({
-    mutationFn: login,
-    onError: () => {
-      notifications.show({
-        title: "Error",
-        message: t("loginFailed"),
-        color: "red",
-      });
-    },
-    onSuccess: async ({ data }) => {
-      try {
-        if (data.pwdChangeRequired) {
-          router.push(`/auth/change-password/${data.id}`);
-        } else {
-          logger.log(data);
-
-          const user = await getUserAction(data.access_token);
-          if (user) {
-            setUser({
-              ...user,
-              role: data.role as "administrator" | "work_provider",
-            } as User);
-          }
-
-          router.push(redirectPath);
-
-          //   notifications.show({
-          //     title: "Success",
-          //     message: t("loginSuccess"),
-          //     color: "green",
-          //   });
-          setCookie("role", data.role);
-          setCookie(COOKIE_ACCESS_TOKEN, data.access_token, {
-            maxAge: rememberMe ? 7 * 24 * 60 * 60 : undefined,
-          });
-          setCookie(COOKIE_REFRESH_TOKEN, data.access_token, {
-            httpOnly: true,
-            secure: true,
-            maxAge: rememberMe ? 30 * 24 * 60 * 60 : undefined,
-          });
+    useEffect(() => {
+        const returnTo = searchParams.get('returnTo');
+        if (returnTo) {
+            setRedirectPath(decodeURIComponent(returnTo));
         }
-      } catch (error) {
-        logger.error("Error processing login success:", error);
-      }
-    },
-  });
+    }, [searchParams]);
 
-  const onSubmit = (values: { email: string; password: string }) => {
-    loginMutation.mutateAsync({
-      username: values.email,
-      password: values.password,
-      origin: "portal",
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ resolver: zodResolver(schema) });
+
+    const loginMutation = useMutation({
+        mutationFn: login,
+        onError: () => {
+            notifications.show({
+                title: 'Error',
+                message: t('loginFailed'),
+                color: 'red',
+            });
+        },
+        onSuccess: async ({ data }) => {
+            try {
+                if (data.pwdChangeRequired) {
+                    router.push(`/auth/change-password/${data.id}`);
+                } else {
+                    logger.log(data);
+
+                    const user = await getUserAction(data.access_token);
+                    if (user) {
+                        setUser({
+                            ...user,
+                            role: data.role as
+                                | 'administrator'
+                                | 'work_provider',
+                        } as User);
+                    }
+
+                    router.push(redirectPath);
+
+                    //   notifications.show({
+                    //     title: "Success",
+                    //     message: t("loginSuccess"),
+                    //     color: "green",
+                    //   });
+                    setCookie('role', data.role);
+                    setCookie(COOKIE_ACCESS_TOKEN, data.access_token, {
+                        maxAge: rememberMe ? 7 * 24 * 60 * 60 : undefined,
+                    });
+                    setCookie(COOKIE_REFRESH_TOKEN, data.access_token, {
+                        httpOnly: true,
+                        secure: true,
+                        maxAge: rememberMe ? 30 * 24 * 60 * 60 : undefined,
+                    });
+                }
+            } catch (error) {
+                logger.error('Error processing login success:', error);
+            }
+        },
     });
-  };
 
-  return (
-    <Box className="flex items-center justify-center bg-white shadow rounded w-full md:w-1/2">
-      <div className="relative w-full p-8">
-        <Flex direction={"column"} align="center">
-          <Title className="text-xl text-start">{t("title")}</Title>
-          <Text ta="start" className="mb-3 text-gray-500 text-sm">
-            {t("subtitle")}
-          </Text>
-        </Flex>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Stack gap="md">
-            <TextInput
-              label={t("emailLabel")}
-              placeholder={t("emailPlaceholder")}
-              {...register("email")}
-              error={errors.email?.message}
-            />
-            <PasswordInput
-              label={t("passwordLabel")}
-              placeholder={t("passwordPlaceholder")}
-              {...register("password")}
-              error={errors.password?.message}
-            />
-            <Group justify="flex-end" mt={"sm"}>
-              {/* <Checkbox
+    const onSubmit = (values: { email: string; password: string }) => {
+        loginMutation.mutateAsync({
+            username: values.email,
+            password: values.password,
+            origin: 'portal',
+        });
+    };
+
+    return (
+        <Box className="flex items-center justify-center bg-white shadow rounded w-full md:w-1/2">
+            <div className="relative w-full p-8">
+                <Flex direction={'column'} align="center">
+                    <Title className="text-xl text-start">{t('title')}</Title>
+                    <Text ta="start" className="mb-3 text-gray-500 text-sm">
+                        {t('subtitle')}
+                    </Text>
+                </Flex>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Stack gap="md">
+                        <TextInput
+                            label={t('emailLabel')}
+                            placeholder={t('emailPlaceholder')}
+                            {...register('email')}
+                            error={errors.email?.message}
+                        />
+                        <PasswordInput
+                            label={t('passwordLabel')}
+                            placeholder={t('passwordPlaceholder')}
+                            {...register('password')}
+                            error={errors.password?.message}
+                        />
+                        <Group justify="flex-end" mt={'sm'}>
+                            {/* <Checkbox
                                 title="Remember me"
                                 label={t('rememberMe')}
                                 className="text-teal-600"
@@ -154,27 +156,29 @@ const Login = () => {
                                     setRememberMe(e.target.checked)
                                 }
                             /> */}
-              <Anchor
-                size="sm"
-                onClick={() => router.push("/auth/forgot-password")}
-                className="text-sm text-teal-600 hover:underline"
-              >
-                {t("forgotPassword")}
-              </Anchor>
-            </Group>
-            <Button
-              type="submit"
-              fullWidth
-              className="w-full rounded-md px-4 py-2 text-white mt-4"
-              loading={loginMutation.isPending}
-            >
-              {t("loginButton")}
-            </Button>
-          </Stack>
-        </form>
-      </div>
-    </Box>
-  );
+                            <Anchor
+                                size="sm"
+                                onClick={() =>
+                                    router.push('/auth/forgot-password')
+                                }
+                                className="text-sm text-teal-600 hover:underline"
+                            >
+                                {t('forgotPassword')}
+                            </Anchor>
+                        </Group>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            className="w-full rounded-md px-4 py-2 text-white mt-4"
+                            loading={loginMutation.isPending}
+                        >
+                            {t('loginButton')}
+                        </Button>
+                    </Stack>
+                </form>
+            </div>
+        </Box>
+    );
 };
 
 export default Login;
--- apps/frontend/portal/app/[locale]/layout.tsx
+++ apps/frontend/portal/app/[locale]/layout.tsx
@@ -1,73 +1,78 @@
-import { logger } from "@shega/shared";
-import { AuthProvider } from "@shega/ui";
-import { getUserAction } from "app/_api/get-user-action";
-import type { Metadata } from "next";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { Inter } from "next/font/google";
-import { cookies } from "next/headers";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
-import MantineThemeProvider from "providers/MantineProviders";
-import QueryProviders from "providers/Query.provider";
+import { logger } from '@shega/shared';
+import { AuthProvider } from '@shega/ui';
+import { getUserAction } from 'app/_api/get-user-action';
+import type { Metadata } from 'next';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { Inter } from 'next/font/google';
+import { cookies } from 'next/headers';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import MantineThemeProvider from 'providers/MantineProviders';
+import QueryProviders from 'providers/Query.provider';
 
-import type { ReactNode } from "react";
-import { cn } from "utility/cn";
-import { generateColors, lightenHexColor } from "utility/colors";
+import type { ReactNode } from 'react';
+import { cn } from 'utility/cn';
+import { generateColors, lightenHexColor } from 'utility/colors';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Shega Jobs",
-  description:
-    "Empowering Job Seekers and connecting them to the best of the best",
+    title: 'Shega Jobs',
+    description:
+        'Empowering Job Seekers and connecting them to the best of the best',
 };
 
 //bluish theme
-const defaultTheme = "#004c4c";
+const defaultTheme = '#004c4c';
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: ReactNode;
+    children: ReactNode;
 }) {
-  const cookieValues = await cookies();
-  const messages = await getMessages();
-  const user = await getUserAction();
+    const cookieValues = await cookies();
+    const messages = await getMessages();
+    const user = await getUserAction();
 
-  const role = cookieValues.get("role")?.value as
-    | "administrator"
-    | "work_provider";
-  logger.log({ user, role });
+    const role = cookieValues.get('role')?.value as
+        | 'administrator'
+        | 'work_provider';
+    logger.log({ user, role });
 
-  const colorArray = generateColors(defaultTheme);
-  const styles: Record<string, string> = {
-    "--primary-color-default": defaultTheme,
-    "--primary-radius": "8px",
-    backgroundColor: lightenHexColor(defaultTheme, 98) || "#004c4c",
-  };
+    const colorArray = generateColors(defaultTheme);
+    const styles: Record<string, string> = {
+        '--primary-color-default': defaultTheme,
+        '--primary-radius': '8px',
+        backgroundColor: lightenHexColor(defaultTheme, 98) || '#004c4c',
+    };
 
-  colorArray.forEach((color, index) => {
-    styles[`--primary-color-${index}`] = color;
-  });
-  return (
-    <html lang="en">
-      <body
-        className={cn("h-screen w-full", inter.className)}
-        style={styles}
-        suppressHydrationWarning={true}
-      >
-        <NuqsAdapter>
-          <NextIntlClientProvider messages={messages}>
-            <QueryProviders>
-              <MantineThemeProvider color={defaultTheme} radius={"8px"}>
-                <AuthProvider user={user ? { ...user, role } : undefined}>
-                  {children}
-                </AuthProvider>
-              </MantineThemeProvider>
-            </QueryProviders>
-          </NextIntlClientProvider>
-        </NuqsAdapter>
-      </body>
-    </html>
-  );
+    colorArray.forEach((color, index) => {
+        styles[`--primary-color-${index}`] = color;
+    });
+    return (
+        <html lang="en">
+            <body
+                className={cn('h-screen w-full', inter.className)}
+                style={styles}
+                suppressHydrationWarning={true}
+            >
+                <NuqsAdapter>
+                    <NextIntlClientProvider messages={messages}>
+                        <QueryProviders>
+                            <MantineThemeProvider
+                                color={defaultTheme}
+                                radius={'8px'}
+                            >
+                                <AuthProvider
+                                    user={user ? { ...user, role } : undefined}
+                                >
+                                    {children}
+                                </AuthProvider>
+                            </MantineThemeProvider>
+                        </QueryProviders>
+                    </NextIntlClientProvider>
+                </NuqsAdapter>
+            </body>
+        </html>
+    );
 }
