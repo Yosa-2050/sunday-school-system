58215d6565adddb549ab2f68764964e43ccb12f3
DawitMezgebu <dawitmezgebu22@gmail.com>
2025-03-25 17:37:58 +0300

    payload update declinereason to note

--- apps/frontend/office/app/[locale]/_api/admin/decline-jobs.ts
+++ apps/frontend/office/app/[locale]/_api/admin/decline-jobs.ts
@@ -1,11 +1,11 @@
 import { fetcher } from '@shega/shared';
 
-export const declineJob = async (id: string, declineReason: string) => {
+export const declineJob = async (id: string, note: string) => {
     const response = await fetcher(`/job-portal/decline/${id}`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            declineReason,
+            note,
         }),
     });
 
