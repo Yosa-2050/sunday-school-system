b519d1669cb10d03f12d9dc69ed6f5dabb623f66
Mintesinot <mysecondemail7923@gmail.com>
2025-04-22 08:08:26 +0000

    Sjp 143 add email templates for job application and organization

--- apps/backend/api/src/notification/seeds/notificationTemplates.seed.ts
+++ apps/backend/api/src/notification/seeds/notificationTemplates.seed.ts
@@ -1,6 +1,10 @@
 import { NotificationChannel } from '../enums/notification-channel.enum';
 import { getForgotPwdEmailTemplate } from './templates/forgotPwdEmailTemplate';
 import { getJobApplicationEmailTemplate } from './templates/jobApplicationEmailTemplate';
+import { getJobPostApprovedEmailTemplate } from './templates/jobPostApprovedEmailTemplate';
+import { getJobPostDeclinedEmailTemplate } from './templates/jobPostDeclinedEmailTemplate';
+import { getOrgRegistrationApprovedEmailTemplate } from './templates/orgRegistrationApprovedEmailTemplate';
+import { getOrgRegistrationDeclinedEmailTemplate } from './templates/orgRegistrationDeclinedEmailTemplate';
 import { getSignupEmailTemplate } from './templates/signupEmailTemplate';
 
 export const NotificationTemplatesSeedData = [
@@ -22,6 +26,34 @@ export const NotificationTemplatesSeedData = [
         subject:
             'Confirmation of Your Job Application for {{jobTitle}} at {{companyName}}',
         content: getCleanedHtmlString(getJobApplicationEmailTemplate()),
+    },
+    {
+        channelType: NotificationChannel.Email,
+        templateName: 'jobPostApprovedEmailTemplate',
+        subject: 'Job Posting Approved - {{jobTitle}}',
+        content: getCleanedHtmlString(getJobPostApprovedEmailTemplate()),
+    },
+    {
+        channelType: NotificationChannel.Email,
+        templateName: 'jobPostDeclinedEmailTemplate',
+        subject: 'Job Posting Declined - {{jobTitle}}',
+        content: getCleanedHtmlString(getJobPostDeclinedEmailTemplate()),
+    },
+    {
+        channelType: NotificationChannel.Email,
+        templateName: 'orgRegistrationApprovedEmailTemplate',
+        subject: 'Organization Registration Approved - {{Organization Name}}',
+        content: getCleanedHtmlString(
+            getOrgRegistrationApprovedEmailTemplate(),
+        ),
+    },
+    {
+        channelType: NotificationChannel.Email,
+        templateName: 'orgRegistrationDeclinedEmailTemplate',
+        subject: 'Organization Registration Declined - {{Organization Name}}',
+        content: getCleanedHtmlString(
+            getOrgRegistrationDeclinedEmailTemplate(),
+        ),
     },
 ];
 
