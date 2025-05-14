3bbd2788d0042bc4a44e95fb2a954b93a6596cbf
eyoelfikadu <eyoel.fikadu@equitybank.co.ke>
2025-05-14 12:26:45 +0300

    biome

--- apps/backend/api/src/users/enums/allEnums.ts
+++ apps/backend/api/src/users/enums/allEnums.ts
@@ -1,5 +1,8 @@
+import { CommitmentType } from '@shega/job_portal/enums/commitment-type.enum';
 import { EducationalRequirmentType } from '@shega/job_portal/enums/education-requirment-type.enum';
 import { EmploymentType } from '@shega/job_portal/enums/employment-type.enum';
+import { ExperianceLevelType } from '@shega/job_portal/enums/experiance-level-type.enum';
+import { MentorshipType } from '@shega/job_portal/enums/mentorship-type.enum';
 import { SalaryFrequencyType } from '@shega/job_portal/enums/salary-frequency-type.enum';
 import { SalaryType } from '@shega/job_portal/enums/salary-type.enum';
 import { WorkPlaceType } from '@shega/job_portal/enums/work-place-type.enum';
@@ -9,9 +12,6 @@ import { Gender } from './profile-gender.enum';
 import { MarriageStatus } from './profile-marriagestatus.enum';
 import { Title } from './profile-title.enum';
 import { UserRoleType } from './user-role.enum';
-import { CommitmentType } from '@shega/job_portal/enums/commitment-type.enum';
-import { MentorshipType } from '@shega/job_portal/enums/mentorship-type.enum';
-import { ExperianceLevelType } from '@shega/job_portal/enums/experiance-level-type.enum';
 
 export const AllEnums = {
     LoginBy,
