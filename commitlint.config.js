58602219f9a17e1fc0b28fca6b3f2f71cee3806f
Ermias <wteffera11@gmail.com>
2025-06-14 17:41:24 +0300

    feat(shega): setup app versioning

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 100],
+        'footer-max-line-length': [2, 'always', 100],
+    },
+    ignores: [
+        (commit) =>
+            commit.includes('[skip ci]') ||
+            commit.includes('chore(release):') ||
+            commit.startsWith('Merge'),
+    ],
+};
