d1cfb04b32ed04121bc7a4d721c92b02c513fcd6
eyoelfikadu <eyoel.fikadu@equitybank.co.ke>
2025-11-13 17:51:58 +0300

    add home room entity

---
+++ apps/backend/api/src/lms/entities/home-room.entity.ts
@@ -0,0 +1,18 @@
+import { BaseModel } from '@shega/Utilities/entities/base-model.entity';
+import { OrganizationMembers } from '@shega/organization/entities/organization-member.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+// biome-ignore lint/style/useImportType: <explanation>
+import { TeacherType } from '../enums/teacher-type.enums';
+import { Classes } from './classes.entity';
+
+@Entity()
+export class HomeroomAssignment extends BaseModel {
+    @ManyToOne(() => Classes, { eager: true })
+    class: Classes;
+
+    @ManyToOne(() => OrganizationMembers, { eager: true })
+    member: OrganizationMembers;
+
+    @Column()
+    type: TeacherType;
+}
