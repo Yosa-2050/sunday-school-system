37b3da50865f30223d3e52dc060ded8dd7cb44a0
yoseph <yoseph7091@gmail.com>
2026-03-23 10:58:17 -0100

    feat: sms integration and bulk sms

--- apps/backend/api/src/notification/notification.service.ts
+++ apps/backend/api/src/notification/notification.service.ts
@@ -32,6 +32,8 @@ export class NotificationService {
         private readonly emailService: IEmailService,
         @Inject(ISmsServiceInterface)
         private readonly smsService: ISmsService,
+        @Inject(ISmsServiceInterface)
+        private readonly smsService: ISmsService,
         @InjectRepository(Notification)
         private notificationRepo: Repository<Notification>,
         @InjectRepository(NotificationTemplate)
