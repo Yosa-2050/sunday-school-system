11006c1cb4a88802f4cf0836a53e86c5b50962f6
yoseph <yoseph7091@gmail.com>
2026-03-28 04:49:22 -0100

    feat: fixed

--- apps/backend/api/src/events/entity/event-member.entity.ts
+++ apps/backend/api/src/events/entity/event-member.entity.ts
@@ -1,13 +1,13 @@
 import { BaseModel } from "@shega/Utilities/entities/base-model.entity";
-import { ManyToOne } from "typeorm";
+import { Entity, ManyToOne } from "typeorm";
 import { Event } from "./event.entity";
 import { OrganizationMembers } from "@shega/organization/entities/organization-member.entity";
 
+@Entity()
 export class EventMember extends BaseModel {
-    @ManyToOne(() => Event, {eager: true})
-    event: Event;
+  @ManyToOne(() => Event, { eager: true })
+  event: Event;
 
-     @ManyToOne(() => OrganizationMembers, {eager: true})
-    member: OrganizationMembers;
-
+  @ManyToOne(() => OrganizationMembers, { eager: true })
+  member: OrganizationMembers;
 }
